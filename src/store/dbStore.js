2e35088c03da641b09d9b0c66e66d45889d930fc
hogsmill <info@hogsmill.com>
2020-11-08 17:27:13 +0000

    update default backlog

--- src/store/dbStore.js
+++ src/store/dbStore.js
@@ -8,10 +8,10 @@ const defaultEstimationValues = {
 }
 
 const defaultBacklog = [
-  { id: 1, title: 'Build an API', description: 'Build the API by updating the domain model to allow feature x to connect'},
-  { id: 2, title: 'Do the UI', description: 'Update the GUI to allow product selection. Date comes from the API'},
-  { id: 3, title: 'Build the login', description: 'Store user details in database'},
-  { id: 4, title: 'Add Logging', description: 'Update logging system to log new feature'}
+  { id: 1, uid: '1', title: 'Build an API', description: 'Build the API by updating the domain model to allow feature x to connect'},
+  { id: 2, uid: '2', title: 'Do the UI', description: 'Update the GUI to allow product selection. Date comes from the API'},
+  { id: 3, uid: '3', title: 'Build the login', description: 'Store user details in database'},
+  { id: 4, uid: '4', title: 'Add Logging', description: 'Update logging system to log new feature'}
 ]
 
 const defaultTeams = [
