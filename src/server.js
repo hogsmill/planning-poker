e33fdc06ef095a1d6b65437e21c362fda3cc5191
hogsmill <info@hogsmill.com>
2021-08-25 11:38:25 +0100

    upgrade

--- src/server.js
+++ src/server.js
@@ -25,6 +25,9 @@ ON_DEATH(function(signal, err) {
   })
 })
 
+global.TextEncoder = require("util").TextEncoder
+global.TextDecoder = require("util").TextDecoder
+
 let httpServer
 let io
 if (!prod) {
